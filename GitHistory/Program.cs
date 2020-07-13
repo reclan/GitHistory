1b8ac936964d6de475ba7b14df2de9e616ca022f
Constantine Kolobkov <reclan@hotmail.com>
2020-07-13 15:08:47 +0300

    Add project files.

---
+++ GitHistory/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GitHistory
+{
+    class Program
+    {
+        const string SEPARATOR      = "===END===";
+        const string COMMIT_PATTERN = @"^([^)]*)(?:\(([^)]*?)\)|):(.*?(?:\[([^\]]+?)\]|))\s*$";
+        const string FORMAT         = "%H%n%s%n%b%n" + SEPARATOR;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        private static string GetLastTag()
+        {
+            return null;
+        }   
+    }
+}
